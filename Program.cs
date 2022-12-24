703ac0209ec274c3ffc33058ad44ebc590402505
Alexey <redy22@mail.ru>
2022-12-24 15:19:49 +0400

    new2

--- Program.cs
+++ Program.cs
@@ -3,28 +3,28 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-// int n = InputInt("Введите положительное число: ");
-// int m = 1;
-// if (n < 1)
-// {
-//     Console.WriteLine("Ввели не положительное число: ");
-// }
-// Console.WriteLine(naturalNum(n, m));
+int n = InputInt("Введите положительное число: ");
+int m = 1;
+if (n < 1)
+{
+    Console.WriteLine("Ввели не положительное число: ");
+}
+Console.WriteLine(naturalNum(n, m));
 
-// int naturalNum(int n, int m)
-// {
-//     if (n == m)
-//         return n;
-//     else
-//         Console.Write($"{naturalNum(n, m + 1)}, ");
-//     return m;
-// }
+int naturalNum(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{naturalNum(n, m + 1)}, ");
+    return m;
+}
 
-// int InputInt(string output)
-// {
-//     Console.Write(output);
-//     return int.Parse(Console.ReadLine());
-// }
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
 
 
 // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
@@ -32,22 +32,22 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-// int m = InputInt("Введите M: ");
-// int n = InputInt("Введите N: ");
-// Console.WriteLine($"Сумма элементов от {m} до {n} = {CountNaturalSum(m, n)}");
+int m = InputInt("Введите M: ");
+int n = InputInt("Введите N: ");
+Console.WriteLine($"Сумма элементов от {m} до {n} = {CountNaturalSum(m, n)}");
 
-// int InputInt(string output)
-// {
-//     Console.Write(output);
-//     return int.Parse(Console.ReadLine());
-// }
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
 
-// int CountNaturalSum(int m, int n)
-// {
-//     if (m == n)
-//         return n;
-//     return n + CountNaturalSum(m, n - 1);
-// }
+int CountNaturalSum(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + CountNaturalSum(m, n - 1);
+}
 
 
 // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
